1fc433a296612fc99f86c2b2727b5ba8db1e03a8
tatajanaa <37144309+tatajanaa@users.noreply.github.com>
2019-12-19 12:23:38 +0100

    Update TestConnection.java

--- src/main/java/data_access/TestConnection.java
+++ src/main/java/data_access/TestConnection.java
@@ -1,9 +1,9 @@
 package data_access;
 
-import controllers.CO2Controller;
-import controllers.HumidityController;
 import controllers.TempController;
+import data_access.JDBC_connection.DataWarehouseConnection;
 import data_access.JDBC_connection.SourceDbConnection;
+import webservice.SmartCellarService;
 
 import java.sql.SQLException;
 import java.text.ParseException;
@@ -12,15 +12,15 @@ import java.text.SimpleDateFormat;
 public class TestConnection {
 
     public static void main(String[] args) throws ParseException, SQLException {
+        final SimpleDateFormat SDF = new SimpleDateFormat("dd-MM-yyyy");
+        java.sql.Date startDate1 = new java.sql.Date(SDF.parse("01-12-2019").getTime());
+        java.sql.Date endDate1 = new java.sql.Date(SDF.parse("03-12-2019").getTime());
+
+        TempController c = new TempController();
+        c.getAverageTemperaturePerHour(startDate1);
 
 
-        ThresholdsRepo t = new ThresholdsRepo(SourceDbConnection.getConnection());
-        t.setCO2Thresholds(500, 1000);
-
-
-
-
-
+        //System.out.println(r.getReadings(startDate1, endDate1));
 
 
     }
